7d58ab25f12f8a36a7cf302e0219528af69c51b9
SyedSajib <56638411+Syed18103392@users.noreply.github.com>
2023-09-16 20:08:15 +0600

    Testimonial Carousel
    Using Owl Carousel

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+
+        //Basic Speeds
+        slideSpeed: 200,
+        paginationSpeed: 800,
+
+        //Autoplay
+        autoPlay: false,
+        goToFirst: true,
+        goToFirstSpeed: 1000,
+
+        // Navigation
+        navigation: false,
+        navigationText: ["prev", "next"],
+        pagination: true,
+        paginationNumbers: true,
+
+        // Responsive
+        responsive: true,
+        items: 4,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1]
+
+    })
+});
